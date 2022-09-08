74e372889ce2a0050df2b1ca92e7f1f7a8879f00
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-09-09 00:02:11 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+* main - prints "Programming is like
+*building a multilingual puzzle"
+*Retun: 0 if successfully compiled, non-zero otherwise
+*/
+int main(void)
+{
+        puts("\"Programming is like bilding a multilingual puzzle");
+         return (0)
+  }
