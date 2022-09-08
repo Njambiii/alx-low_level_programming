a44da32311f27e12d4b5a3822f6228a75cf8df2d
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-09-09 00:10:08 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
 * main - prints "Programming is like
-*building a multilingual puzzle"
-*Retun: 0 if successfully compiled, non-zero otherwise
+* building a multilingual puzzle"
+* Return: 0 if successfully compiled, non-zero otherwise
 */
 int main(void)
 {
-        puts("\"Programming is like bilding a multilingual puzzle");
-         return (0)
+        puts("\"Programming is like building a multilingual puzzle");
+         return (0);
   }
