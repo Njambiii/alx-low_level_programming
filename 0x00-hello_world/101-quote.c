5aae6e01ec8e4af86d83dd6109afa72377f66205
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-09-09 00:32:10 +0300

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints this "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: 0 if successfully compiled, non-zero otherwise
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
